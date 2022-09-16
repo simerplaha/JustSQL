edc44cc0ea5a2e22ed9d68bd5febc401e514a0c8
simerplaha <simer.j@gmail.com>
2022-09-17 06:25:32 +1000

    `JustSQLCommonSpec`

--- justsql/src/test/scala/justsql/JustSQLCommonSpec.scala
+++ justsql/src/test/scala/justsql/JustSQLCommonSpec.scala
@@ -33,67 +33,77 @@ trait JustSQLCommonSpec extends AnyWordSpec {
   def connector(): SQLConnector
 
   "update" should {
-    "create and insert" when {
+    "create a table and insert" when {
       "not transactional" in {
-        withDB(connector()) { implicit db =>
-          "CREATE TABLE TEST_TABLE (value varchar)".update().runSync().success.value shouldBe 0
-          "INSERT INTO TEST_TABLE values ('value1')".update().runSync().success.value shouldBe 1
+        withDB(connector()) {
+          implicit db =>
+            //create table
+            "CREATE TABLE TEST_TABLE (value varchar)".update().runSync().success.value shouldBe 0
+            //insert data to table
+            "INSERT INTO TEST_TABLE values ('value1')".update().runSync().success.value shouldBe 1
 
-          UpdateSQL {
-            implicit param =>
-              s"INSERT INTO TEST_TABLE values (${"value2".?})"
-          }.runSync().success.value shouldBe 1
+            //another insert with parameters
+            UpdateSQL {
+              implicit param =>
+                s"INSERT INTO TEST_TABLE values (${"value2".?})"
+            }.runSync().success.value shouldBe 1
+
+            //check data exists
+            "SELECT * FROM TEST_TABLE".select[String]().runSync().success.value shouldBe ArraySeq("value1", "value2")
         }
       }
 
       "transactional" when {
         "parametrised" in {
-          withDB(connector()) { implicit db =>
-            UpdateSQL {
-              implicit param =>
-                s"""
-                   |BEGIN;
-                   |
-                   |CREATE TABLE TEST_TABLE (value INT);
-                   |INSERT INTO TEST_TABLE values (${1.?}), (${2.?});
-                   |INSERT INTO TEST_TABLE values (${3.?}), (${4.?});
-                   |
-                   |COMMIT;
-                   |""".stripMargin
-            }.runSync().success.value shouldBe 0
+          withDB(connector()) {
+            implicit db =>
+              UpdateSQL {
+                implicit param =>
+                  s"""
+                     |BEGIN;
+                     |
+                     |CREATE TABLE TEST_TABLE (value INT);
+                     |INSERT INTO TEST_TABLE values (${1.?}), (${2.?});
+                     |INSERT INTO TEST_TABLE values (${3.?}), (${4.?});
+                     |
+                     |COMMIT;
+                     |""".stripMargin
+              }.runSync().success.value shouldBe 0
 
-            "SELECT * from TEST_TABLE".select[Int]().runSync().success.value shouldBe (1 to 4)
+              "SELECT * from TEST_TABLE".select[Int]().runSync().success.value shouldBe (1 to 4)
           }
         }
 
         "not parametrised" in {
-          withDB(connector()) { implicit db =>
-            """
-              |BEGIN;
-              |
-              |CREATE TABLE TEST_TABLE (value Int);
-              |INSERT INTO TEST_TABLE values (1), (2), (3);
-              |
-              |COMMIT;
-              |"""
-              .stripMargin
-              .update().runSync().success.value shouldBe 0
+          withDB(connector()) {
+            implicit db =>
+              """
+                |BEGIN;
+                |
+                |CREATE TABLE TEST_TABLE (value Int);
+                |INSERT INTO TEST_TABLE values (1), (2), (3);
+                |
+                |COMMIT;
+                |"""
+                .stripMargin
+                .update().runSync().success.value shouldBe 0
 
-            "SELECT * from TEST_TABLE".select[Int]().runSync().success.value shouldBe (1 to 3)
+              "SELECT * from TEST_TABLE".select[Int]().runSync().success.value shouldBe (1 to 3)
           }
         }
       }
 
       "insert tuple" in {
-        withDB(connector()) { implicit db =>
-          "CREATE TABLE TEST_TABLE (value varchar, int INT)".update().runSync().success.value shouldBe 0
+        withDB(connector()) {
+          implicit db =>
+            "CREATE TABLE TEST_TABLE (value varchar, int INT)".update().runSync().success.value shouldBe 0
 
-          UpdateSQL {
-            implicit param =>
-              s"INSERT INTO TEST_TABLE values (${"String" ?}, ${1 ?})"
-          }.runSync().success.value shouldBe 1
+            UpdateSQL {
+              implicit param =>
+                s"INSERT INTO TEST_TABLE values (${"String" ?}, ${1 ?})"
+            }.runSync().success.value shouldBe 1
 
-          "SELECT * from TEST_TABLE".select[(String, Int)]().runSync().success.value should contain only (("String", 1))
+            "SELECT * from TEST_TABLE".select[(String, Int)]().runSync().success.value should contain only (("String", 1))
         }
       }
     }
@@ -101,118 +111,120 @@ trait JustSQLCommonSpec extends AnyWordSpec {
 
 
   "return empty select on empty table" in {
-    withDB(connector()) { implicit db =>
-      "CREATE TABLE TEST_TABLE(key varchar)".update()
+    withDB(connector()) {
+      implicit db =>
+        "CREATE TABLE TEST_TABLE(key varchar)".update().runSync().success.value shouldBe 0
+        "SELECT * FROM TEST_TABLE".select[String]().headOption().runSync().success.value shouldBe empty
 
-      "CREATE TABLE TEST_TABLE(key varchar)".update().runSync().success.value shouldBe 0
+        /** SELECT */
+        //Select using typed API
+        "SELECT * FROM TEST_TABLE".select[String]().runSync().success.value shouldBe empty
+        //Select using Java ResultSet
+        "SELECT * FROM TEST_TABLE".unsafeSelect[String](_.getString("key")).runSync().success.value shouldBe empty
 
-      "SELECT * FROM TEST_TABLE".select[String]().headOption().runSync().success.value shouldBe empty
-
-      /** SELECT */
-      //Select using typed API
-      "SELECT * FROM TEST_TABLE".select[String]().runSync().success.value shouldBe empty
-      //Select using Java ResultSet
-      "SELECT * FROM TEST_TABLE".unsafeSelect[String](_.getString("key")).runSync().success.value shouldBe empty
-
-      /** COUNT */
-      //Count using typed API
-      "SELECT count(*) FROM TEST_TABLE".select[Int]().headOption().runSync().success.value should contain(0)
+        /** COUNT */
+        //Count using typed API
+        "SELECT count(*) FROM TEST_TABLE".select[Int]().headOption().runSync().success.value should contain(0)
     }
   }
 
   "return non-empty select on non-empty table" in {
-    withDB(connector()) { implicit db =>
-      "CREATE TABLE TEST_TABLE(key varchar)".update().runSync().success.value shouldBe 0
-      "INSERT INTO TEST_TABLE values ('1'), ('2'), ('3')".update().runSync().success.value shouldBe 3
+    withDB(connector()) {
+      implicit db =>
+        "CREATE TABLE TEST_TABLE(key varchar)".update().runSync().success.value shouldBe 0
+        "INSERT INTO TEST_TABLE values ('1'), ('2'), ('3')".update().runSync().success.value shouldBe 3
 
-      /** SELECT */
-      //Select using typed API
-      "SELECT * FROM TEST_TABLE".select[String]().runSync().success.value shouldBe ArraySeq("1", "2", "3")
-      //Select using Java ResultSet
-      "SELECT * FROM TEST_TABLE".unsafeSelect[String](_.getString("key")).runSync().success.value shouldBe ArraySeq("1", "2", "3")
+        /** SELECT */
+        //Select using typed API
+        "SELECT * FROM TEST_TABLE".select[String]().runSync().success.value shouldBe ArraySeq("1", "2", "3")
+        //Select using Java ResultSet
+        "SELECT * FROM TEST_TABLE".unsafeSelect[String](_.getString("key")).runSync().success.value shouldBe ArraySeq("1", "2", "3")
 
-      /** COUNT */
-      //Count using typed API
-      "SELECT count(*) FROM TEST_TABLE".select[Int]().headOption().runSync().success.value should contain(3)
+        /** COUNT */
+        //Count using typed API
+        "SELECT count(*) FROM TEST_TABLE".select[Int]().headOption().runSync().success.value should contain(3)
     }
   }
 
   "return zero for count query when table shouldBe empty" in {
-    withDB(connector()) { implicit db =>
-      "CREATE TABLE TEST_TABLE(key varchar)".update().runSync().success.value shouldBe 0
+    withDB(connector()) {
+      implicit db =>
+        "CREATE TABLE TEST_TABLE(key varchar)".update().runSync().success.value shouldBe 0
 
-      /** COUNT */
-      //Count using typed API
-      "SELECT count(*) FROM TEST_TABLE".select[Int]().headOption().runSync().success.value should contain(0)
-      //Count using typed API with naming column
-      "SELECT count(*) as count FROM TEST_TABLE".select[Int]().headOption().runSync().success.value should contain(0)
-      //Count using ResultSet
-      "SELECT count(*) as count FROM TEST_TABLE".unsafeSelect[Int](_.getInt("count")).headOption().runSync().success.value should contain(0)
+        /** COUNT */
+        //Count using typed API
+        "SELECT count(*) FROM TEST_TABLE".select[Int]().headOption().runSync().success.value should contain(0)
+        //Count using typed API with naming column
+        "SELECT count(*) as count FROM TEST_TABLE".select[Int]().headOption().runSync().success.value should contain(0)
+        //Count using ResultSet
+        "SELECT count(*) as count FROM TEST_TABLE".unsafeSelect[Int](_.getInt("count")).headOption().runSync().success.value should contain(0)
     }
   }
 
   "return row count when table shouldBe non-empty" in {
-    withDB(connector()) { implicit db =>
-      "CREATE TABLE TEST_TABLE(key varchar)".update().runSync().success.value shouldBe 0
-      "INSERT INTO TEST_TABLE values ('one'), ('two'), ('three')".update().runSync().success.value shouldBe 3
+    withDB(connector()) {
+      implicit db =>
+        "CREATE TABLE TEST_TABLE(key varchar)".update().runSync().success.value shouldBe 0
+        "INSERT INTO TEST_TABLE values ('one'), ('two'), ('three')".update().runSync().success.value shouldBe 3
 
-      /** COUNT */
-      //Count using typed API
-      "SELECT count(*) FROM TEST_TABLE".select[Int]().headOption().runSync().success.value should contain(3)
-      //Count using typed API with naming column
-      "SELECT count(*) as count FROM TEST_TABLE".select[Int]().headOption().runSync().success.value should contain(3)
-      //Count using ResultSet
-      "SELECT count(*) as count FROM TEST_TABLE".unsafeSelect(_.getInt("count")).headOption().runSync().success.value should contain(3)
+        /** COUNT */
+        //Count using typed API
+        "SELECT count(*) FROM TEST_TABLE".select[Int]().headOption().runSync().success.value should contain(3)
+        //Count using typed API with naming column
+        "SELECT count(*) as count FROM TEST_TABLE".select[Int]().headOption().runSync().success.value should contain(3)
+        //Count using ResultSet
+        "SELECT count(*) as count FROM TEST_TABLE".unsafeSelect(_.getInt("count")).headOption().runSync().success.value should contain(3)
     }
   }
 
   "select from table with multiple rows created using transactions" in {
-    withDB(connector()) { implicit db =>
+    withDB(connector()) {
+      implicit db =>
 
-      """
-        |BEGIN;
-        |
-        |CREATE TABLE TEST_TABLE(
-        |   int SERIAL PRIMARY KEY,
-        |   string VARCHAR NOT NULL,
-        |   bool BOOLEAN NOT NUll
-        |);
-        |
-        |INSERT INTO TEST_TABLE
-        |values (0, 'string1', 'true'),
-        |       (1, 'string2', 'false'),
-        |       (2, 'string3', 'true');
-        |
-        |COMMIT;
-        |""".stripMargin.update().runSync().success.value shouldBe 0
+        """
+          |BEGIN;
+          |
+          |CREATE TABLE TEST_TABLE(
+          |   int SERIAL PRIMARY KEY,
+          |   string VARCHAR NOT NULL,
+          |   bool BOOLEAN NOT NUll
+          |);
+          |
+          |INSERT INTO TEST_TABLE
+          |values (0, 'string1', 'true'),
+          |       (1, 'string2', 'false'),
+          |       (2, 'string3', 'true');
+          |
+          |COMMIT;
+          |""".stripMargin.update().runSync().success.value shouldBe 0
 
-      case class Row(int: Int, string: String, bool: Boolean)
+        case class Row(int: Int, string: String, bool: Boolean)
 
-      implicit val rowReader: RowReader[Row] = RowReader(Row.tupled)
+        implicit val rowReader: RowReader[Row] = RowReader(Row.tupled)
 
-      /** SELECT USING A CASE CLASS */
-      "SELECT * FROM TEST_TABLE".select[Row]().runSync().success.value shouldBe
-        ArraySeq(
-          Row(int = 0, string = "string1", bool = true),
-          Row(int = 1, string = "string2", bool = false),
-          Row(int = 2, string = "string3", bool = true)
-        )
+        /** SELECT USING A CASE CLASS */
+        "SELECT * FROM TEST_TABLE".select[Row]().runSync().success.value shouldBe
+          ArraySeq(
+            Row(int = 0, string = "string1", bool = true),
+            Row(int = 1, string = "string2", bool = false),
+            Row(int = 2, string = "string3", bool = true)
+          )
 
-      /** SELECT USING A TUPLE */
-      "SELECT * FROM TEST_TABLE".select[(Int, String, Boolean)]().runSync().success.value shouldBe
-        ArraySeq(
-          (0, "string1", true),
-          (1, "string2", false),
-          (2, "string3", true)
-        )
+        /** SELECT USING A TUPLE */
+        "SELECT * FROM TEST_TABLE".select[(Int, String, Boolean)]().runSync().success.value shouldBe
+          ArraySeq(
+            (0, "string1", true),
+            (1, "string2", false),
+            (2, "string3", true)
+          )
 
-      /** COUNT */
-      //Count using typed API
-      "SELECT count(*) FROM TEST_TABLE".select[Int]().headOption().runSync().success.value should contain(3)
-      //Count using typed API with naming column
-      "SELECT count(*) as count FROM TEST_TABLE".select[Int]().headOption().runSync().success.value should contain(3)
-      //Count using ResultSet
-      "SELECT count(*) as count FROM TEST_TABLE".unsafeSelect[Int](_.getInt("count")).headOption().runSync().success.value should contain(3)
+        /** COUNT */
+        //Count using typed API
+        "SELECT count(*) FROM TEST_TABLE".select[Int]().headOption().runSync().success.value should contain(3)
+        //Count using typed API with naming column
+        "SELECT count(*) as count FROM TEST_TABLE".select[Int]().headOption().runSync().success.value should contain(3)
+        //Count using ResultSet
+        "SELECT count(*) as count FROM TEST_TABLE".unsafeSelect[Int](_.getInt("count")).headOption().runSync().success.value should contain(3)
     }
   }
 
@@ -231,7 +243,7 @@ trait JustSQLCommonSpec extends AnyWordSpec {
                |""".stripMargin
         }.runSync().success.value shouldBe 3
 
-        val maxIntQuery: SelectSQL[Int, ArraySeq] =
+        val maxIntQuery =
           SelectSQL[Int] {
             implicit params: Params =>
               s"""
@@ -239,7 +251,7 @@ trait JustSQLCommonSpec extends AnyWordSpec {
                  |""".stripMargin
           }
 
-        val finalQuery: TrackedSQL[Option[Int]] =
+        val finalQuery =
           SelectSQL[Int] {
             implicit params: Params =>
               s"""
